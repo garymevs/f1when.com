b281f34b0c62507130d6131d275ffbadffcfbece
garymevs <garymevs@googlemail.com>
2022-10-24 19:57:39 +0000

    json endpoint and error handling

--- main.go
+++ main.go
@@ -6,120 +6,158 @@ import (
 	"fmt"
 	"html/template"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
-// Function for handling requests to /
-func webRoot(w http.ResponseWriter, req *http.Request) {
-    fmt.Println("Request from:", req.RemoteAddr)
-    // If we haven't pulled the data from ergast in 24 hours then pull it again (Might increse this in the future)
-    if time.Now().After(dataPullTime.Add(time.Hour*24)) {
-        fmt.Println(time.Now(), "is more than 24 hours after", dataPullTime, "reloading data")
-        loadSeasonData()
-    }
-    // Get the next race (duh!)
-    race, err := getNextRace(&raceTable)
-    if err != nil {
-        fmt.Fprintf(w, err.Error())
-    }
-    // Render the page template using the race retrieved in the previous function
-    pageTemplate.Execute(w, &Page{race})
-}
-
-// Reload the page template then return the main page 
-func templateReload(w http.ResponseWriter, req *http.Request) {
-    loadPageTemplate()
-    webRoot(w, req)
-}
-
-// True to enable dev features
-var devMode = false
-
-// Store the data in a publicly accessible area
-var baseData BaseData
-var raceTable RaceTable
+// Main HTML page template
 var pageTemplate *template.Template
 
-// Store when we last pulled the data
+// F1 season data
+var raceTable RaceTable
 var dataPullTime time.Time
 
+const dataRefreshPeriod time.Duration = time.Hour * 24
+
 func main() {
-    // Load the page template
-    loadPageTemplate()
+	// check for DEV env variable
+	devMode, err := strconv.ParseBool(os.Getenv("DEV"))
+	if err != nil {
+		devMode = false
+	}
 
-    // Download the json data from ergast.com
-    loadSeasonData()
+	// Load the page template
+	pageTemplate, err = loadPageTemplate("template/page-template.html")
+	if err != nil {
+		log.Fatalf("error loading page template: %s", err)
+	}
 
-    // Setup data objects 
-    f1Data := baseData.MRData
-    raceTable = f1Data.RaceTable
+	// Download the json data from ergast.com
+	refreshRaceTable()
 
-    // Serve static resources
-    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-    // Serve the main page 
-    http.HandleFunc("/", webRoot)
-    // Dev mode pages
-    if devMode {
-        http.HandleFunc("/reload-template", templateReload)
-    }
-    // Start the web server
-    if devMode {
-        http.ListenAndServe(":8081", nil)
-        fmt.Println("Dev mode server started on :8081")
-    } else {
-        http.ListenAndServe(":8080", nil)
-        fmt.Println("Server started on :8080")
-    }
+	// Serve static resources
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	// Serve the main page
+	http.HandleFunc("/json", jsonRoot)
+	http.HandleFunc("/", webRoot)
+	// Dev mode pages
+	if devMode {
+		http.HandleFunc("/reload-template", templateReload)
+	}
+	// Start the web server
+	if devMode {
+		fmt.Println("Starting dev server on :8081")
+		http.ListenAndServe(":8081", nil)
+	} else {
+		fmt.Println("Starting server on :8080")
+		http.ListenAndServe(":8080", nil)
+	}
 }
 
-func loadPageTemplate() {
-    fmt.Println("Loading page template...")
-    // Load template file
-    template, err := template.ParseFiles("template/page-template.html")
-    if err != nil {
-        fmt.Println(err.Error())
-    }
-    pageTemplate = template
-    fmt.Println("Page template load complete")
+func refreshRaceTable() {
+	seasonData, err := loadSeasonData()
+	if err != nil {
+		log.Fatalf("error loading season data: %s", err)
+	}
+	// Update dataPullTime
+	dataPullTime = time.Now()
+
+	// Setup data objects
+	raceTable = seasonData.MRData.RaceTable
 }
 
 // Download the JSON from ergast.com
-func loadSeasonData() {
-    fmt.Println("Requesting data from ergast...")
-    // Data from here: https://ergast.com/api/f1/current.json
-    // Create http client and make GET request
-    httpClient := &http.Client{}
-    res, err := httpClient.Get("https://ergast.com/api/f1/current.json")
-    if err != nil {
-        fmt.Println("Error pulling data from ergast")
-        fmt.Println(err.Error())
-        os.Exit(1)
-    }
-    defer res.Body.Close()
+func loadSeasonData() (BaseData, error) {
+	log.Println("Requesting data from ergast...")
+	baseData := BaseData{}
+	// Data from here: https://ergast.com/api/f1/current.json
+	// Create http client and make GET request
+	httpClient := &http.Client{}
+	res, err := httpClient.Get("https://ergast.com/api/f1/current.json")
+	if err != nil {
+		return baseData, err
+	}
+	defer res.Body.Close()
 
-    // Read the data and unmarshal it to the structs in f1data.go
-    byteValue, _ := ioutil.ReadAll(res.Body)
-    json.Unmarshal(byteValue, &baseData)
+	// Read the data and unmarshal it to the structs in f1data.go
+	byteValue, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return baseData, err
+	}
 
-    // Update dataPullTime
-    dataPullTime = time.Now()
-    fmt.Println("Data pull complete at:", dataPullTime)
+	// Unmarshal read bytes into JSON object
+	err = json.Unmarshal(byteValue, &baseData)
+	if err != nil {
+		return baseData, err
+	}
+
+	log.Println("Data pull complete")
+	return baseData, nil
+}
+
+func loadPageTemplate(pageFilePath string) (*template.Template, error) {
+	log.Println("Loading page template...")
+	template, err := template.ParseFiles(pageFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load template file: %s", pageFilePath)
+	}
+	log.Println("Page template load complete")
+	return template, nil
 }
 
 // Iterate over the Races in the RaceTable and find the first race with a date after today
 func getNextRace(raceTable *RaceTable) (*Race, error) {
-    for _, race := range raceTable.Races {
-        parsedDate, err := time.Parse("2006-01-02 15:04:05Z", race.Date + " " + race.Time)
-        if err != nil {
-            fmt.Println(err.Error())
-            break
-        }
+	for _, race := range raceTable.Races {
+		parsedDate, err := time.Parse("2006-01-02 15:04:05Z", race.Date+" "+race.Time)
+		if err != nil {
+			log.Println(err.Error())
+			break
+		}
 
-        if time.Now().Before(parsedDate.Add(time.Hour * 2)) {
-            return &race, nil
-        }
-    }
-    return nil, errors.New("Couldn't find next race")
+		// Return the race if the time now is before the race + 2 hours
+		// Example: If the race starts at 3PM it will still be returned until 5PM
+		if time.Now().Before(parsedDate.Add(time.Hour * 2)) {
+			return &race, nil
+		}
+	}
+	return nil, errors.New("couldn't find next race")
+}
+
+// Function for handling requests to /
+func webRoot(w http.ResponseWriter, req *http.Request) {
+	log.Printf("Request from: %s", req.RemoteAddr)
+	// If we haven't pulled the data from ergast in 24 hours then pull it again (Might increse this in the future)
+	if time.Now().After(dataPullTime.Add(dataRefreshPeriod)) {
+		log.Println(time.Now(), "is more than 24 hours after", dataPullTime, "reloading data")
+		refreshRaceTable()
+	}
+	race, err := getNextRace(&raceTable)
+	if err != nil {
+		fmt.Fprintf(w, "error loading next race: %s", err.Error())
+	}
+
+	// Render the page template using the race retrieved in the previous function
+	pageTemplate.Execute(w, &Page{race})
+}
+
+func jsonRoot(w http.ResponseWriter, req *http.Request) {
+	if time.Now().After(dataPullTime.Add(dataRefreshPeriod)) {
+		log.Println(time.Now(), "is more than 24 hours after", dataPullTime, "reloading data")
+		refreshRaceTable()
+	}
+	race, err := getNextRace(&raceTable)
+	if err != nil {
+		fmt.Fprintf(w, "error loading next race: %s", err.Error())
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(race)
+}
+
+// Reload the page template then return the main page
+func templateReload(w http.ResponseWriter, req *http.Request) {
+	refreshRaceTable()
+	webRoot(w, req)
 }
